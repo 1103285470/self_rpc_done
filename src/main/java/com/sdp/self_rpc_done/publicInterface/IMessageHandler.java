7d8f2b9fbc298718195c7809b151bba74bca4729
sundongpo_i <sundongpo_i@didiglobal.com>
2020-05-05 18:03:37 +0800

    none

---
+++ src/main/java/com/sdp/self_rpc_done/publicInterface/IMessageHandler.java
@@ -0,0 +1,1 @@
+package com.sdp.self_rpc_done.publicInterface;import io.netty.channel.ChannelHandlerContext;public interface IMessageHandler<T> {    void handle(ChannelHandlerContext ctx, String requestId, T message);}
